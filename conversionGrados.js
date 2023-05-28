43d9ef3cfe27f18d243857007c5809e9b84b7075
Adán Lain Bertero <merlybertero@gmail.com>
2023-05-28 20:29:52 +0000

    Agrego el archivo conversionGrados.js

---
+++ conversionGrados.js
@@ -0,0 +1,11 @@
+let fahrenheit = 50;
+let celsius = (fahrenheit - 32) * 5/9;
+
+console.log(celsius);
+//El resultado es 10
+
+fahrenheit = 75;
+celsius = (fahrenheit - 32) * 5/9;
+
+console.log(celsius);
+//EL resultado es 23.88888888888889
