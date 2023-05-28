da5a384c4f90637a0fc3da91b77ea56c3885bf5c
Adán Lain Bertero <merlybertero@gmail.com>
2023-05-28 20:30:26 +0000

    Practico N0 finalizado

---
+++ areaRectangulo.js
@@ -0,0 +1,10 @@
+let base = 5;
+let altura = 3;
+let area = base * altura;
+console.log(area);
+//El resultado es 15
+base = 12,5;
+altura = 3;
+area = base * altura;
+console.log(area);
+//El resultdo es 66.25
---
+++ promedio3.js
@@ -0,0 +1,18 @@
+let x = 5;
+let y = 7;
+let z = 9;
+let promedio1 = (x + y + z) / 3;
+console.log(promedio1);
+//El promedio es 7
+x = 5;
+y = 7;
+z = 9;
+let promedio2 = (x + y + z) / 3;
+console.log(promedio2);
+//El promedio es -4.333333333333333
+x = -1.2;
+y = 5.3;
+z = -3.3;
+let promedio3 = (x + y + z) / 3;
+console.log(promedio3);
+//El promedio es 0.26666666666666666
