505525dbf898a416a9f0f8b67d6e2a0adccf7177
Adán Lain Bertero <merlybertero@gmail.com>
2023-06-12 14:39:23 -0300

    practico1

---
+++ practico1/frutas.js
@@ -0,0 +1,5 @@
+let frutas = ["Manzana", "Banana", "Sandia", "Naranja", "Uva"]
+let ultima = frutas[frutas.length - 1]
+console.log("La ultima fruta agregada fue: " + ultima);
+let primera = frutas[0]
+console.log("La primera fruta agregada fue: " + primera);
---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,4 @@
+let nombre = "Adán";
+let apellido = "Bertero";
+let edad = 20;
+console.log ("Hola, " + nombre + " " + apellido + ". Tienes " + edad + " años.");
